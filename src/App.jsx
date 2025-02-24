9d31c1a2a99f93fec8560c47780c1de19c5b5260
MdShahinDev <hello.shahinofficial@gmail.com>
2025-02-24 15:06:08 +0600

    Path Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home';
-import Shop from './pages/Shop';
-import Product from './pages/Product';
-import Wishlist from './pages/Wishlist';
-import MyAccount from './pages/MyAccount';
+
 import Header from './components/Header';
+import Home from './Pages/Home';
+import Shop from './Pages/Shop';
+import Product from './Pages/Product';
+import Wishlist from './Pages/Wishlist';
+import MyAccount from './Pages/MyAccount';
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/shop' element={<Shop/>}/>
-        <Route path='/product' element={<Product/>} />
-        <Route path='/wishlist' element={<Wishlist/>} />
-        <Route path='/my-account' element={<MyAccount/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path='/my-account' element={<MyAccount />} />
       </Routes>
-
     </div>
   );
 }
