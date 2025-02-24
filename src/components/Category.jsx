02dfc526800b747a2c383b14a29b5a27be6ff319
MdShahinDev <hello.shahinofficial@gmail.com>
2025-02-25 01:08:24 +0600

    Category Complete with image

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from 'react';
+import cat1 from '../assets/category/cat-1.webp'
+import cat2 from '../assets/category/cat-2.webp'
+import cat3 from '../assets/category/cat-3.webp'
+import cat4 from '../assets/category/cat-4.webp'
+import cat5 from '../assets/category/cat-5.webp'
+import cat6 from '../assets/category/cat-6.webp'
 const Category = () => {
   return (
-    <div className='container mx-auto responsive-padding my-2'>
-        <h2>Category</h2>
+    <div className='container mx-auto responsive-padding my-2 py-2'>
+      <div className='grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6'>
+        <div className='single-cat flex flex-col justify-center items-center py-4 shadow-lg rounded-lg border border-gray-200 cursor-pointer'>
+          <img src={cat1}  alt='' className='w-[100px] h-auto rounded-lg ' />
+          <p className='text-center mt-2'>Category 1</p>
+        </div>
+        <div className='single-cat flex flex-col justify-center items-center py-4 shadow-lg rounded-lg border border-gray-200 cursor-pointer'>
+          <img src={cat2}  alt='' className='w-[100px] h-auto rounded-lg ' />
+          <p className='text-center mt-2'>Category 1</p>
+        </div>
+        <div className='single-cat flex flex-col justify-center items-center py-4 shadow-lg rounded-lg border border-gray-200 cursor-pointer'>
+          <img src={cat3}  alt='' className='w-[100px] h-auto rounded-lg ' />
+          <p className='text-center mt-2'>Category 1</p>
+        </div>
+        <div className='single-cat flex flex-col justify-center items-center py-4 shadow-lg rounded-lg border border-gray-200 cursor-pointer'>
+          <img src={cat4}  alt='' className='w-[100px] h-auto rounded-lg ' />
+          <p className='text-center mt-2'>Category 1</p>
+        </div>
+        <div className='single-cat flex flex-col justify-center items-center py-4 shadow-lg rounded-lg border border-gray-200 cursor-pointer'>
+          <img src={cat5}  alt='' className='w-[100px] h-auto rounded-lg ' />
+          <p className='text-center mt-2'>Category 1</p>
+        </div>
+        <div className='single-cat flex flex-col justify-center items-center py-4 shadow-lg rounded-lg border border-gray-200 cursor-pointer'>
+          <img src={cat6}  alt='' className='w-[100px] h-auto rounded-lg ' />
+          <p className='text-center mt-2'>Category 1</p>
+        </div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
